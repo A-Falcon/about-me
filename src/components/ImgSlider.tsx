60615ab3148539a338df8f11724389e92e5be7d3
Ahtziri <Afalcon1209@gmail.com>
2022-03-16 16:24:22 -0700

    create ImgSlider File

---
+++ src/components/ImgSlider.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const ImgSlider: React.FC = () => {
+  return (
+    <Wrapper>
+    </Wrapper>
+  )
+}
+
+
+const Wrapper = styled.div`
+  flex: 1;
+`
+
+export default ImgSlider
