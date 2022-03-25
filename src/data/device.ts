784e6c5788b9648a5970e77787c5d259fc050271
Ahtziri <Afalcon1209@gmail.com>
2022-03-25 11:34:18 -0700

    tweak device size

--- src/data/device.ts
+++ src/data/device.ts
@@ -1,11 +1,11 @@
 const size = {
   xs: '420px',
-  sm: '768px',
+  sm: '820px',
   lg: '1200px',
 }
 
 export const device = {
   xs: `(max-width: ${size.xs})`,
-  sm: `(min-width: ${size.sm})`,
-  lg: `(min-width: ${size.lg})`,
+  sm: `(max-width: ${size.sm})`,
+  lg: `(max-width: ${size.lg})`,
 };
