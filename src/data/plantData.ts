3d8c451e4098679b46fca4efe69d33e77f06ca6e
Ahtziri <Afalcon1209@gmail.com>
2022-03-26 12:33:39 -0700

    add plantData

---
+++ src/data/plantData.ts
@@ -0,0 +1,28 @@
+export const plantData = [
+  {
+    id: 'p1',
+    name: 'Monstera Deliciosa',
+    img: require('../assets/monstera.png')
+  },
+  {
+    id: 'p2',
+    name: 'Arrowhead',
+    img: require('../assets/arrowHead.png')
+  },
+  {
+    id: 'p3',
+    name: 'Basil',
+    img: require('../assets/basil.png')
+  },
+  {
+    id: 'p4',
+    name: 'Pothos',
+    img: require('../assets/fish.png')
+  },
+  {
+    id: 'p5',
+    name: 'Wandering Dude',
+    img: require('../assets/wandering-dude.png')
+  },
+  
+]
